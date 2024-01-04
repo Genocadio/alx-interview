d8f22ad3ac557a1c23171a44ce6f32b5f4138645
geno <yvesgeno@outlook.com>
2024-01-05 00:47:01 +0200

    task 0 correction

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Function that returnspascals triangle list"""
+"""Function that returns Pascal's triangle list"""
+
 
 def pascal_triangle(n):
-  if n <= 0:
-    return []
+    if n <= 0:
+        return []
 
-  triangle = []
-  for i in range(n):
-    row = [1] * (i + 1)
-    for j in range(1, i):
-      row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-    triangle.append(row)
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            triangle.append(row)
 
-  return triangle
+    return triangle
