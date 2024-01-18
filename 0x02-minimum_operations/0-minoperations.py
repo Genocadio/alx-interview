c683328aa366833bc3f557231b42eee3dfad3d6f
geno <yvesgeno@outlook.com>
2024-01-18 23:15:58 +0200

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''function that calaculate the fewest number of operations'''
+
+
+def minOperations(n: int) -> int:
+    '''function that calaculate the fewest number of operations'''
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return operations
