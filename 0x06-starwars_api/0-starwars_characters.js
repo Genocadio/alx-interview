973fc8382b5361d7e094a456f05f3eec5fdbc5eb
geno <yvesgeno@outlook.com>
2024-02-16 00:55:31 +0200

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,34 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`; // Updated API URL
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+
+    const printCharacterNames = (index) => {
+      if (index >= characters.length) {
+        return;
+      }
+
+      const characterUrl = characters[index];
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+          printCharacterNames(index + 1);
+        }
+      });
+    };
+
+    printCharacterNames(0);
   }
-);
+});
