c9783bf66e5db5f734c6b984c713b89dcd994416
geno <yvesgeno@outlook.com>
2024-02-16 00:54:29 +0200

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const film = JSON.parse(body);
-    const characters = film.characters;
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-});
+);
