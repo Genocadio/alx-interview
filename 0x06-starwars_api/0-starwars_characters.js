47d855008334154d7172fac077b914d668a0daa2
geno <yvesgeno@outlook.com>
2024-02-16 01:10:55 +0200

    task 0 correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
+const Id = process.argv[2];
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const apiUrl = `https://swapi.dev/api/films/${Id}/`;
 
 request(apiUrl, (err, response, body) => {
   if (err) {
